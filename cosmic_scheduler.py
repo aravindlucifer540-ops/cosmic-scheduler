29e669e6f294564e3c40d750f17124ff9cd791e6
aravindlucifer540-ops <aravindlucifer540@gmail.com>
2026-03-15 23:45:15 +0530

    Update app.run to use environment variable for port

--- cosmic_scheduler.py
+++ cosmic_scheduler.py
@@ -1413,4 +1413,5 @@ if __name__ == '__main__':
     print("  ► Data saved locally in: schedule_data.json")
     print("  ► Press Ctrl+C to stop the server")
     print("═"*55 + "\n")
-    app.run(debug=False, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
